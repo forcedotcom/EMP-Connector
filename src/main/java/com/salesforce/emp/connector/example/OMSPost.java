dd79f5f234bb7034ac1d5dc3c60105a7355e80e3
Yegor Lopatin <DK3331_admin@falck.com>
2019-10-10 18:30:22 +0200

    OMS sync: fix OMSPost time format

--- src/main/java/com/salesforce/emp/connector/example/OMSPost.java
+++ src/main/java/com/salesforce/emp/connector/example/OMSPost.java
@@ -27,7 +27,7 @@ public class OMSPost {
 		String url = "";
 
  		ZonedDateTime now = ZonedDateTime.now();
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss GMT");
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O");
         String nowStr = f.format(now);
 
 		// String for signing the key
