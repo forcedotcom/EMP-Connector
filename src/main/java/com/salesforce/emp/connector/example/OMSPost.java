8798c045e7cc543c77f5266fb47b56b6993f5162
Yegor Lopatin <DK3331_admin@falck.com>
2019-10-10 17:37:44 +0200

    OMS sync: fix OMSPost time format

--- src/main/java/com/salesforce/emp/connector/example/OMSPost.java
+++ src/main/java/com/salesforce/emp/connector/example/OMSPost.java
@@ -26,6 +26,9 @@ public class OMSPost {
 		// String for signing the key
 		String stringToSign="POST\n" + json.length() + "\napplication/json\nx-ms-date:" + nowStr + "\n/api/logs";
 
+		System.out.println("\nDEBUG. stringToSign : " + stringToSign);
+		System.out.println("\nDEBUG. message : "  + json);
+
 		try {
 			byte[] decodedBytes = Base64.decodeBase64(shared_key);
 
@@ -36,6 +39,9 @@ public class OMSPost {
 			encodedHash = DatatypeConverter.printBase64Binary(hash);
 			Signature = "SharedKey " + customer_id + ":" + encodedHash;
 	    
+			System.out.println("\nDEBUG. Signature : " + Signature);
+
+
 			url = "https://" + customer_id + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";	    
 			URL objUrl = new URL(url);
 			HttpsURLConnection con = (HttpsURLConnection) objUrl.openConnection();
@@ -55,8 +61,8 @@ public class OMSPost {
 			System.out.println("\nSending 'POST' request to URL : " + url);
 			System.out.println("\nSalesforce channel ID : " + clientSessionChannel_Id);
 			System.out.println("\nSalesforce message  status : " + flag);
-			System.out.println("Post parameters : " + json);
-			System.out.println("Response Code : " + responseCode);
+			System.out.println("\nPost parameters : " + json);
+			System.out.println("\nResponse Code : " + responseCode);
 		}
 		catch (Exception e) {
 			System.out.println("Catch statement: " + e);
