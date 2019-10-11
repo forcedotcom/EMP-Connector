09e2adf42ec895d7625b8bf5c9299a0296f3f8a7
Yegor Lopatin <DK3331_admin@falck.com>
2019-10-11 14:58:37 +0200

    Example of redirecting logs to Azure Log Analytics

---
+++ src/main/java/com/salesforce/emp/connector/example/OMSPost.java
@@ -0,0 +1,70 @@
+package com.salesforce.emp.connector.example;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+import java.io.DataOutputStream;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import javax.xml.bind.DatatypeConverter;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class OMSPost {
+
+	public static void main(String args[]) {
+
+		String customer_id = args[0];
+		String shared_key = args[1];
+		String log_type = args[2];
+		String json = args[3];
+
+		String Signature = "";
+		String encodedHash = "";
+		String url = "";
+
+ 		ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O");
+        String nowStr = f.format(now);
+
+		// String for signing the key
+		String stringToSign = "POST\n" + json.length() + "\napplication/json\nx-ms-date:" + nowStr + "\n/api/logs";
+
+		try {
+			byte[] decodedBytes = Base64.decodeBase64(shared_key);
+
+			Mac hasher = Mac.getInstance("HmacSHA256");
+			hasher.init(new SecretKeySpec(decodedBytes, "HmacSHA256"));
+			byte[] hash = hasher.doFinal(stringToSign.getBytes());
+		    
+			encodedHash = DatatypeConverter.printBase64Binary(hash);
+			Signature = "SharedKey " + customer_id + ":" + encodedHash;
+
+			url = "https://" + customer_id + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";	    
+			URL objUrl = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) objUrl.openConnection();
+			con.setDoOutput(true);
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Log-Type", log_type);
+			con.setRequestProperty("x-ms-date", nowStr);
+			con.setRequestProperty("Authorization", Signature);
+	        
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(json);
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("\nPosted log message : " + json);
+			System.out.println("\nResponse Code : " + responseCode);
+		}
+		catch (Exception e) {
+			System.out.println("Catch statement: " + e);
+		}
+	}
+}
