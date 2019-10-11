7e57d3ff854fec5560de3e4ce01ecb43d2688f26
Yegor Lopatin <DK3331_admin@falck.com>
2019-10-11 08:32:08 +0200

    fix timezone format

--- src/main/java/com/salesforce/emp/connector/example/OMSRedirectExample.java
+++ src/main/java/com/salesforce/emp/connector/example/OMSRedirectExample.java
@@ -47,7 +47,7 @@ public class OMSRedirectExample {
         BayeuxParameters params = tokenProvider.login();
 
         EmpConnector connector = new EmpConnector(params);
-        String json_log = "[" + JSON.toString(event) + "]"
+        String json_log = "[" + JSON.toString(event) + "]";
         OMSLoggingListener loggingListener = new OMSLoggingListener(true, true, argv[4],  argv[5], argv[6], json_log);
 
         connector.addListener(META_HANDSHAKE, loggingListener)
