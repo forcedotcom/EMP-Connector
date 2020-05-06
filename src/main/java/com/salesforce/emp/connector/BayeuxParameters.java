d89b86a3804089c061a0879fc62cc42af9c2c762
Lawrence <admin@lmcalpin.com>
2020-05-06 12:34:48 -0400

    The CometD client option for the max response size should be 'maxMessageSize'

--- src/main/java/com/salesforce/emp/connector/BayeuxParameters.java
+++ src/main/java/com/salesforce/emp/connector/BayeuxParameters.java
@@ -60,7 +60,7 @@ public interface BayeuxParameters {
     default Map<String, Object> longPollingOptions() {
         Map<String, Object> options = new HashMap<>();
         options.put("maxNetworkDelay", maxNetworkDelay());
-        options.put("maxBufferSize", maxBufferSize());
+        options.put("maxMessageSize", maxBufferSize());
         return options;
     }
 
